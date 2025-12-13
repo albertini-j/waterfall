203325896eaad4409f0c69a2bc1f4323b7507354
albertini-j <66831089+albertini-j@users.noreply.github.com>
2025-12-13 18:22:54 -0300

    Add delay handling and simplify waterfall import

---
+++ src/waterfall/__init__.py
@@ -0,0 +1,6 @@
+"""Biblioteca simples para planejamento de projetos waterfall."""
+
+from .activity import Activity
+from .project import ProjectSchedule
+
+__all__ = ["Activity", "ProjectSchedule"]
