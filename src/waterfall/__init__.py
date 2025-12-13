237149430aca4240744ae9fe880002ed61fb792e
albertini-j <66831089+albertini-j@users.noreply.github.com>
2025-12-13 20:32:30 -0300

    Delete src/waterfall directory

--- src/waterfall/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Biblioteca simples para planejamento de projetos waterfall."""
-
-from .activity import Activity
-from .project import ProjectSchedule
-
-__all__ = ["Activity", "ProjectSchedule"]
